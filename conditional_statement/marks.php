c915fe9714c2c1048ea79c60d6319e53e1ebb2d6
rafisgithub <rafi.cse.ahmed@gmail.com>
2026-05-22 12:43:36 +0600

    switch case

---
+++ conditional_statement/marks.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$total_marks = 85;
+$grade = "A-";
+$gpa = "3.5";
+
+if ($total_marks > 40 && $total_marks <= 45) {
+    echo "You got an ".$grade." grade and your GPA is ".$gpa.".";
+
+}else if ($total_marks > 45 && $total_marks <= 50) {
+    echo 
+
+}else if ($total_marks > 50 && $total_marks <= 55) {
+
+
+}else if ($total_marks > 55 && $total_marks <= 60) {
+
+}else if ($total_marks > 60 && $total_marks <= 65) {
+
+}else if ($total_marks > 65 && $total_marks <= 70) {
+
+}else if ($total_marks > 70 && $total_marks <= 75) {
+    
+}else if ($total_marks > 75 && $total_marks <= 80) {
+
+}
+
+?>
---
+++ conditional_statement/switch_condition.php
@@ -0,0 +1,31 @@
+<?php
+
+$day = "Monday";
+
+switch ($day) {
+    case "Monday":
+        echo "Today is Monday.";
+        break;
+    case "Tuesday":
+        echo "Today is Tuesday.";
+        break;
+    case "Wednesday":
+        echo "Today is Wednesday.";
+        break;
+    case "Thursday":
+        echo "Today is Thursday.";
+        break;
+    case "Friday":
+        echo "Today is Friday.";
+        break;
+    case "Saturday":
+        echo "Today is Saturday.";
+        break;
+    case "Sunday":
+        echo "Today is Sunday.";
+        break;
+    default:
+        echo "Invalid day.";
+}
+
+?>
